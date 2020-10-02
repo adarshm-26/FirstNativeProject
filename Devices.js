9a306f0a3696c603f4fc0a6c96ce82f34f6e53f3
Adarsh Maurya <adarsh.maurya.2699@gmail.com>
2020-10-02 22:18:28 +0530

    Wifi support

--- Devices.js
+++ Devices.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Text, Switch, Title, Subheading, Paragraph } from 'react-native-paper';
-import { TouchableOpacity, View, FlatList, AsyncStorage, ScrollView, Image } from 'react-native';
+import { Text, Switch, Title, Subheading, ActivityIndicator } from 'react-native-paper';
+import { TouchableOpacity, View, FlatList, AsyncStorage, ScrollView, Image, PermissionsAndroid, Alert } from 'react-native';
 import SocketIOClient from 'socket.io-client';
-import { Header } from './Components';
+import { Header, StyledButton, TextInputField } from './Components';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useFocusEffect } from '@react-navigation/native';
-import { AuthContext } from './App';
+import WifiManager from 'react-native-wifi-reborn';
 import theme from './Theme';
 import { SERVER_HOSTNAME, SOCKETIO_PORT } from './config';
+import { getStoredData } from './utils';
 
 const DevicesStack = createStackNavigator();
 
@@ -20,16 +21,16 @@ const DeviceLayout = ({ item, onPress }) => {
       <Title style={{ flex: 2 }}>{item.name}</Title>
       <Subheading 
         style={{ flex: 1, textAlign: 'right' }}>
-          {item.configurationId ? 'Configured' : 'Unconfigured'}
+          {item.configured ? 'Configured' : 'Unconfigured'}
       </Subheading>
     </View>
   </TouchableOpacity>   
   );
 }
 
-const MyDevices = ({ navigation }) => {
+const MyDevices = ({ route, navigation }) => {
   const [devices,setDevices] = React.useState('');
-  
+
   useFocusEffect(React.useCallback(() => {
     AsyncStorage.getItem('authData')
     .then(authData => {
@@ -50,9 +51,28 @@ const MyDevices = ({ navigation }) => {
     return <DeviceLayout
       item={item}
       onPress={(device) => {
-        navigation.navigate('Configure Device', {
-          device: device
-        });
+        if (item.configured) {
+          navigation.navigate('Configure', {
+            device: device
+          });
+        }
+        else {
+          Alert.alert('Prerequisites', 
+            'Make sure your IoT device is turned on', [
+              {
+                text: 'Ok',
+                onPress: () => {
+                  WifiManager.setEnabled(true);
+                },
+                style: 'default'
+              }
+            ],
+            { cancelable: false }
+          );
+          navigation.navigate('Setup', {
+            device: device
+          });
+        }
       }}
     />
   }
@@ -89,7 +109,7 @@ const ConfigureDevice = ({ route, navigation }) => {
       socket.emit('status', route.params.device.configurationId, {...switches});
     });
     socket.on('status', (configurationId, state) => {
-      console.log('Status ' + configurationId + ' ' + JSON.stringify(state));
+      console.log('Status recieved ' + configurationId + ' : ' + JSON.stringify(state));
       if (route.params.device.configurationId === configurationId) {
         setSwitchesState({
           ...state
@@ -154,6 +174,151 @@ const ConfigureDevice = ({ route, navigation }) => {
   );
 }
 
+const SetupDevice = ({ route, navigation }) => {
+  const [availableNetworks, setNetworks] = React.useState([]);
+  const [scanning, setScanning] = React.useState(false);
+  
+  React.useEffect(() => {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location permission is required to scan wifi networks',
+        message:
+          'This app needs location permission as this is required  ' +
+          'to scan for wifi networks.',
+        buttonNegative: 'DENY',
+        buttonPositive: 'ALLOW',
+      }
+    ).then(result => {
+      if (result === PermissionsAndroid.RESULTS.GRANTED) {
+        setScanning(true);
+        WifiManager.loadWifiList()
+        .then(networks => setNetworks(networks))
+        .catch(e => {
+          alert('Could not scan for networks');
+          console.error(e);
+        })
+        .finally(() => {
+          setScanning(false);
+        });  
+      }
+      else {
+        alert('You need to allow location access for this app');
+      }
+    })
+    .catch(e => {
+      console.error(e);
+      alert('Could not scan for networks');
+    })
+  }, []);
+
+  const rescan = () => {
+    setScanning(true);
+    WifiManager.reScanAndLoadWifiList()
+    .then(networks => setNetworks(networks))
+    .catch(e => {
+      alert('Failed, please retry');
+      console.error(e);
+    })
+    .finally(() => {
+      setScanning(false);
+    });
+  }
+
+  return (<View style={{ margin: 5, padding: 10, flex: 1 }}>
+    {
+      scanning ?
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size='large'/>
+      </View> :
+      <View>
+        {
+          availableNetworks.length > 0 ? 
+          availableNetworks.map((network, index) => {
+            return (<TouchableOpacity
+              key={index}
+              onPress={() => navigation.navigate(
+                'Wifi', {
+                  ssid: network.SSID,
+                  device: route.params.device
+                }
+              )}
+            >
+              <Text style={{
+                fontSize: 20,
+                padding: 5
+              }}>{network.SSID}</Text>
+            </TouchableOpacity>);
+          }) :
+          <Text style={{
+            fontSize: 20,
+            padding: 5
+          }}>No networks found</Text>
+        }
+        <StyledButton
+          disabled={scanning}
+          onPress={rescan}
+          inner='Rescan'
+        />
+      </View>
+    }
+  </View>);
+}
+
+const ConfigureWifi = ({ route, navigation }) => {
+  const [password, setPassword] = React.useState('');
+  const [connecting, setConnecting] = React.useState(false);
+
+  const attemptConnection = async () => {
+    setConnecting(true);
+    try{
+      await WifiManager.connectToProtectedSSID(
+        route.params.ssid,
+        password,
+        false
+      );
+      console.log('Connected to ' + route.params.ssid);
+      await AsyncStorage.setItem('wifi', JSON.stringify({
+        ssid: route.params.ssid,
+        password: password
+      }));
+      /**
+       * TODO :
+       * Add actual IoT device configuration here
+       * Also handle signal server after configuration to update database
+       */
+      let userData = await getStoredData();
+      userData.userData.user.devices[route.params.device.configurationId].configured = true;
+      let result = await AsyncStorage.setItem('authData', JSON.stringify(userData));
+      navigation.navigate('My');
+    } catch (e) {
+      alert('Could not connect');
+      console.error(e);
+    } finally {
+      setConnecting(false);
+    }
+  }
+
+  return (
+    <View style={{ padding: 10 }}>
+      <Text style={{
+        fontSize: 20,
+      }}>Enter Password for selected device network below</Text>
+      <TextInputField
+        label='Password'
+        onChangeText={(value) => setPassword(value)}
+        value={password}
+        disabled={connecting}
+      />
+      <StyledButton
+        disabled={connecting}
+        onPress={attemptConnection}
+        inner='Connect'
+      />
+    </View>
+  );
+}
+
 const Devices = ({ route, navigation }) => {
   return (
     <DevicesStack.Navigator
@@ -163,16 +328,26 @@ const Devices = ({ route, navigation }) => {
           <Header {...props} />
         ),
       }}
-      initialRouteName='My Devices'>
+      initialRouteName='My'>
       <DevicesStack.Screen
-        name='My Devices'
+        name='My'
         component={MyDevices}
         options={{ headerTitle: 'My Devices' }}
       />
       <DevicesStack.Screen
-        name='Configure Device'
+        name='Configure'
         component={ConfigureDevice}
         options={{ headerTitle: 'Configure Device' }}
+      />
+      <DevicesStack.Screen
+        name='Setup'
+        component={SetupDevice}
+        options={{ headerTitle: 'Setup Device' }}
+      />
+      <DevicesStack.Screen
+        name='Wifi'
+        component={ConfigureWifi}
+        options={{ headerTitle: 'Configure Wifi' }}
       />
     </DevicesStack.Navigator>
   );
---
+++ config.js
@@ -0,0 +1,3 @@
+export const SERVER_HOSTNAME = 'http://192.168.1.7';
+export const SOCKETIO_PORT = '1337';
+export const SERVER_PORT = '8080';
